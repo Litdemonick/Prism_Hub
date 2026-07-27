16a6d82c0a28c78435b9da572d85a210846944fb
Soul_Of_The_sun <jk3002213@gmail.com>
2026-07-27 01:33:53 -0500

    fix(build): add collection import for firstWhereOrNull en updater nativo

--- lib/utils/application.dart
+++ lib/utils/application.dart
@@ -1,6 +1,8 @@
 ﻿// ignore_for_file: use_build_context_synchronously
 import 'dart:io';
 
+import 'package:collection/collection.dart';
+
 import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
 import 'package:flutter_i18n/flutter_i18n.dart';
